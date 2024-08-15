5f3dce7fb8d6370db761830b4048fb0b23e70fe3
Charles Fiedler <chase.fiedler@skyward.io>
2024-08-15 15:54:13 -0700

    Adding existing project files to Git

---
+++ FetchMeals/FetchMealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchMealsApp.swift
+//  FetchMeals
+//
+//  Created by Charles Fiedler on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchMealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
