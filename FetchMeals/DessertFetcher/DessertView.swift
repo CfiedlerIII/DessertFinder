70c3e8d09869c6a8b16681f66c39896449a33346
Charles Fiedler <chase.fiedler@skyward.io>
2024-08-17 22:17:00 -0700

    feat: added a simple app homepage with navigation feat: added navigation to the fetch dessert page feat: added simple sorting and search filter to the fetch dessert list

--- FetchMeals/DessertFetcher/DessertView.swift
+++ FetchMeals/DessertFetcher/DessertView.swift
@@ -9,22 +9,61 @@ import SwiftUI
 
 struct DessertView: View {
   @ObservedObject var viewModel: DessertViewModel
+  @State private var searchText = ""
 
   var body: some View {
-    VStack {
-      Button(action: {
-        viewModel.fetchDesserts()
-      }, label: {
-        Text("Fetch Desserts")
-      })
-      List {
-        ForEach(viewModel.meals, id: \.id) { meal in
-          MealView(mealData: meal)
-            .listRowSeparator(.hidden)
+    NavigationStack {
+      VStack {
+        Button(action: {
+          viewModel.fetchDesserts()
+        }, label: {
+          Text("Fetch Desserts")
+        })
+        List {
+          ForEach(searchResults, id: \.id) { meal in
+            NavigationLink {
+              Text(meal.name)
+            } label: {
+              MealPreview(mealData: meal)
+            }
+              .listRowSeparator(.hidden)
+              .listRowBackground(Color.clear)
+              .buttonStyle(.plain)
+          }
         }
+        .navigationTitle("Desserts")
+        .searchable(text: $searchText)
+        .listStyle(PlainListStyle())
+        .background(
+          Color(
+            UIColor(
+              red: 0.85,
+              green: 0.85,
+              blue: 0.85,
+              alpha: 1.0
+            )
+          )
+        )
+        .clipShape(RoundedRectangle(cornerRadius: 15.0))
+        .padding()
       }
     }
-    .padding()
+  }
+
+  var searchResults: [MealDataModel] {
+    if searchText.isEmpty {
+      return viewModel.meals.sorted(by: { $0.name < $1.name
+      })
+    } else {
+      let filteredMeals = viewModel.meals.filter {
+        $0.name.contains(searchText)
+      }
+      if !filteredMeals.isEmpty {
+        return filteredMeals.sorted(by: { $0.name < $1.name
+        })
+      }
+      return [MealDataModel(id: "0000", name: "No Results", thumbnail: "")]
+    }
   }
 }
 
